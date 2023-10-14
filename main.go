bec405a5a0eacef3a06aacc61d0596ee8bff9b19
Tristen Mengis <tristen.mengis@gmail.com>
2023-10-14 13:49:17 -0700

    Run the go formatter

--- main.go
+++ main.go
@@ -3,162 +3,162 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "os"
-    "path/filepath"
-    "runtime"
-    "strconv"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
 
-    "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
 // Notes is a type alias for a simple string slice.
-type Notes []string;
+type Notes []string
 
 func main() {
-    command := setup()
+	command := setup()
 
-    if err := command.Execute(); err != nil {
-        log.Fatal(err)
-    }
+	if err := command.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 // setup creates the environment for the command line
 // parser.
 func setup() *cobra.Command {
-    rootCmd := &cobra.Command{
-        Use: "cmd-notes",
-        PersistentPreRun: func(cmd *cobra.Command, args []string) {
-            basePath := getDataBasePath()
+	rootCmd := &cobra.Command{
+		Use: "cmd-notes",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			basePath := getDataBasePath()
 
-            cmd.Root().Annotations["stateFilePath"] = basePath
-        },
-    }
+			cmd.Root().Annotations["stateFilePath"] = basePath
+		},
+	}
 
-    rootCmd.Annotations = make(map[string]string)
+	rootCmd.Annotations = make(map[string]string)
 
-    cmdAdd := &cobra.Command{
-        Use: "add <note to add>",
-        Short: "Add note",
-        Run: addNote,
-    }
+	cmdAdd := &cobra.Command{
+		Use:   "add <note to add>",
+		Short: "Add note",
+		Run:   addNote,
+	}
 
-    cmdRemove := &cobra.Command{
-        Use: "rm <note index to remove>",
-        Short: "Remove note",
-        Run: removeNote,
-    }
+	cmdRemove := &cobra.Command{
+		Use:   "rm <note index to remove>",
+		Short: "Remove note",
+		Run:   removeNote,
+	}
 
-    cmdList := &cobra.Command{
-        Use: "ls",
-        Short: "List notes",
-        Run: listNotes,
-    }
+	cmdList := &cobra.Command{
+		Use:   "ls",
+		Short: "List notes",
+		Run:   listNotes,
+	}
 
-    rootCmd.AddCommand(cmdAdd, cmdList, cmdRemove)
+	rootCmd.AddCommand(cmdAdd, cmdList, cmdRemove)
 
-    return rootCmd
+	return rootCmd
 }
 
 // addNote creates a new note.
 func addNote(cmd *cobra.Command, args []string) {
-    path := cmd.Root().Annotations["stateFilePath"]
-    notes := readState(path)
+	path := cmd.Root().Annotations["stateFilePath"]
+	notes := readState(path)
 
-    notes = append(notes, args[0])
+	notes = append(notes, args[0])
 
-    writeState(path, &notes)    
+	writeState(path, &notes)
 
-    fmt.Println("Successfully added note!")
+	fmt.Println("Successfully added note!")
 }
 
 // removeNote removes an existing note.
 func removeNote(cmd *cobra.Command, args []string) {
-    path := cmd.Root().Annotations["stateFilePath"]
-    notes := readState(path)
+	path := cmd.Root().Annotations["stateFilePath"]
+	notes := readState(path)
 
-    if len(args) < 1 {
-        log.Fatal("specify index to remove")
-    }
+	if len(args) < 1 {
+		log.Fatal("specify index to remove")
+	}
 
-    index, err := strconv.Atoi(args[0])
-    if err != nil {
-        log.Fatal("error parsing note index: ", err)
-    }
+	index, err := strconv.Atoi(args[0])
+	if err != nil {
+		log.Fatal("error parsing note index: ", err)
+	}
 
-    if index >= len(notes) {
-        log.Fatal("invalid note index")
-    }
+	if index >= len(notes) {
+		log.Fatal("invalid note index")
+	}
 
-    notes = append(notes[:index], notes[index + 1:]...)
+	notes = append(notes[:index], notes[index+1:]...)
 
-    writeState(path, &notes)
+	writeState(path, &notes)
 }
 
 // listNotes lists all existing notes.
 func listNotes(cmd *cobra.Command, args []string) {
-    path := cmd.Root().Annotations["stateFilePath"]
-    notes := readState(path)
+	path := cmd.Root().Annotations["stateFilePath"]
+	notes := readState(path)
 
-    for index, note := range notes {
-        fmt.Printf("%d) %s\n", index, note)
-    }
+	for index, note := range notes {
+		fmt.Printf("%d) %s\n", index, note)
+	}
 }
 
 // getDataBasePath finds the directory in which to place the
 // current state of the notes.
 func getDataBasePath() string {
-    var basePath string
-    var exists bool
+	var basePath string
+	var exists bool
 
-    switch runtime.GOOS {
-    case "windows":
-        basePath, exists = os.LookupEnv("LOCALAPPDATA")
-        if !exists {
-            log.Fatal("failed to locate local app data")
-        }
-    case "darwin":
-    case "linux":
-        basePath, exists = os.LookupEnv("XDG_STATE_HOME")
-        if !exists {
-            homeDir, err := os.UserHomeDir()
-            if err != nil {
-                log.Fatal("error locating user's home directory: ", err)
-            }
+	switch runtime.GOOS {
+	case "windows":
+		basePath, exists = os.LookupEnv("LOCALAPPDATA")
+		if !exists {
+			log.Fatal("failed to locate local app data")
+		}
+	case "darwin":
+	case "linux":
+		basePath, exists = os.LookupEnv("XDG_STATE_HOME")
+		if !exists {
+			homeDir, err := os.UserHomeDir()
+			if err != nil {
+				log.Fatal("error locating user's home directory: ", err)
+			}
 
-            basePath = filepath.Join(homeDir, ".local/state")
-        }
-    }
+			basePath = filepath.Join(homeDir, ".local/state")
+		}
+	}
 
-    basePath = filepath.Join(basePath, "cmd-notes")
+	basePath = filepath.Join(basePath, "cmd-notes")
 
-    return basePath
+	return basePath
 }
 
 // readState reads the current state of the notes from
 // the given path.
 func readState(path string) Notes {
-    notes := make([]string, 0)
-    stateFilePath := filepath.Join(path, "state")
+	notes := make([]string, 0)
+	stateFilePath := filepath.Join(path, "state")
 
-    file, err := os.Open(stateFilePath)
-    if os.IsNotExist(err) {
-        return notes
-    } else if err != nil {
-        log.Fatal("error opening state file: ", err) 
-    }
-    defer file.Close()
+	file, err := os.Open(stateFilePath)
+	if os.IsNotExist(err) {
+		return notes
+	} else if err != nil {
+		log.Fatal("error opening state file: ", err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
-    for scanner.Scan() {
-        line := scanner.Text()
-        notes = append(notes, line)
-    }
+	for scanner.Scan() {
+		line := scanner.Text()
+		notes = append(notes, line)
+	}
 
-    return notes
+	return notes
 }
 
 // writeState writes the current state of the notes to
@@ -166,31 +166,31 @@ func readState(path string) Notes {
 // file and then renames it to prevent corruption of
 // the notes.
 func writeState(path string, notes *Notes) {
-    tempFilePath := filepath.Join(path, "state.temp") 
-    stateFilePath := filepath.Join(path, "state")
+	tempFilePath := filepath.Join(path, "state.temp")
+	stateFilePath := filepath.Join(path, "state")
 
-    err := os.MkdirAll(path, 0755)
-    if err != nil {
-        log.Fatal("error creating state directories: ", err) 
-    }
+	err := os.MkdirAll(path, 0755)
+	if err != nil {
+		log.Fatal("error creating state directories: ", err)
+	}
 
-    file, err := os.Create(tempFilePath)
-    if err != nil  {
-        log.Fatal("error creating temporary state file: ", err) 
-    }
+	file, err := os.Create(tempFilePath)
+	if err != nil {
+		log.Fatal("error creating temporary state file: ", err)
+	}
 
-    writer := bufio.NewWriter(file)
+	writer := bufio.NewWriter(file)
 
-    for _, note := range *notes {
-        fmt.Fprintln(writer, note)         
-    }
+	for _, note := range *notes {
+		fmt.Fprintln(writer, note)
+	}
 
-    writer.Flush()
+	writer.Flush()
 
-    file.Close()
+	file.Close()
 
-    err = os.Rename(file.Name(), stateFilePath)
-    if err != nil {
-        log.Fatal("error renaming state file: ", err)
-    }
+	err = os.Rename(file.Name(), stateFilePath)
+	if err != nil {
+		log.Fatal("error renaming state file: ", err)
+	}
 }
