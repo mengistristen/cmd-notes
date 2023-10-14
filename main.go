06fa076c6a343ab41e053f7c545ee3509e6e3760
Tristen Mengis <tristen.mengis@gmail.com>
2023-10-13 18:52:47 -0700

    Stub out some commands

--- main.go
+++ main.go
@@ -18,7 +18,23 @@ func main() {
         },
     }
 
-    rootCmd.AddCommand(cmdAdd)
+    cmdRemove := &cobra.Command{
+        Use: "rm <note id to remove>",
+        Short: "Remove note",
+        Run: func(cmd *cobra.Command, args []string) {
+            fmt.Println("Removing note")
+        },
+    }
+
+    cmdList := &cobra.Command{
+        Use: "ls",
+        Short: "List notes",
+        Run: func(cmd *cobra.Command, args []string) {
+            fmt.Println("Listing notes")
+        },
+    }
+
+    rootCmd.AddCommand(cmdAdd, cmdList, cmdRemove)
 
     if err := rootCmd.Execute(); err != nil {
         fmt.Println(err)
